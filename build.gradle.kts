c51be1dcf56ac80986b2f2bd7e406880edc2dd05
Artur Babichev <artik222012@gmail.com>
2023-07-26 16:08:26 +0400

    disable unused repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ buildscript {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
         classpath(kotlin("gradle-plugin", libs.versions.kotlin.get()))
@@ -11,7 +10,6 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.1.0")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.7")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
         classpath(libs.mokoResources.generator)
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.4.3")
     }
@@ -23,7 +21,6 @@ allprojects {
         mavenCentral()
         maven(url = "https://jitpack.io")
         maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
-        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
